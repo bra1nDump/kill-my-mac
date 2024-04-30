6c058f21b3161396264f65be444f5ffdcb46e10b
Kirill Dubovitskiy <kirill2003de@gmail.com>
2024-04-30 12:12:14 -0700

    Fixing day 0 minute point underflow Adding 2.5 hour warning

--- shutdown-job.sh
+++ shutdown-job.sh
@@ -34,8 +34,19 @@ echo "Current hour:" $current_hour
 echo "Current minute:" $current_minute
 echo "Total minutes from midnight:" $current_minutes
 
+# Warning message 2.5 hours before the dead zone, suggest to start winding down
+warning_two_half_hours_before=$((dead_zone_start_minutes - 150))
+# If the warning time is negative, it means it is the day before the dead zone
+warning_two_half_hours_before=$(( warning_two_half_hours_before < 0 ? 24 * 60 + warning_two_half_hours_before : warning_two_half_hours_before ))
+if [[ $current_minutes == $warning_two_half_hours_before ]]; then
+    echo "2.5 hours before dead zone"
+    say "Warning: The dead zone starts in 2.5 hours. Start winding down your work. Good time to start watching a movie or reading a book."
+fi
+
 # Warning message 1 hour before the dead zone
 warning_one_hour_before=$((dead_zone_start_minutes - 60))
+# If the warning time is negative, it means it is the day before the dead zone
+warning_one_hour_before=$(( warning_one_hour_before < 0 ? 24 * 60 + warning_one_hour_before : warning_one_hour_before ))
 if [[ $current_minutes == $warning_one_hour_before ]]; then
     echo "1 hour before dead zone"
     say "Warning: The dead zone starts in one hour. Prepare to finish up any pending tasks."
@@ -43,6 +54,8 @@ fi
 
 # Warning message 10 minutes before the dead zone suggesting to wrap it up for real now
 warning_ten_minutes_before=$((dead_zone_start_minutes - 10))
+# If the warning time is negative, it means it is the day before the dead zone
+warning_ten_minutes_before=$(( warning_ten_minutes_before < 0 ? 24 * 60 + warning_ten_minutes_before : warning_ten_minutes_before ))
 if [[ $current_minutes == $warning_ten_minutes_before ]]; then
     echo "10 minutes before dead zone"
     say "Warning: The dead zone starts in 10 minutes. Wrap up your work now!"
