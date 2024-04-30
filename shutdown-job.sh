625cdc716a93854824f62bb25fa84faacd75abde
Kirill Dubovitskiy <kirill2003de@gmail.com>
2024-04-29 23:56:33 -0700

    Simplified check

--- shutdown-job.sh
+++ shutdown-job.sh
@@ -34,32 +34,16 @@ echo "Current hour:" $current_hour
 echo "Current minute:" $current_minute
 echo "Total minutes from midnight:" $current_minutes
 
-######## Utilities ########
-
-# Function to check if two values are nearly equal within a given proximity
-is_near() {
-    local value1=$1
-    local value2=$2
-    local proximity=$3
-    # Absolute difference
-    local diff=$((value1 > value2 ? value1 - value2 : value2 - value1))
-
-    # When no return is used, status code from the last command is used
-    # test <expression> is the same as [ <expression> ]
-    # when the test succeeds, 0 status code is returned from the command
-    test $diff -le $proximity
-}
-
 # Warning message 1 hour before the dead zone
 warning_one_hour_before=$((dead_zone_start_minutes - 60))
-if is_near $current_minutes $warning_one_hour_before 0; then
+if [[ $current_minutes == $warning_one_hour_before ]]; then
     echo "1 hour before dead zone"
     say "Warning: The dead zone starts in one hour. Prepare to finish up any pending tasks."
 fi
 
 # Warning message 10 minutes before the dead zone suggesting to wrap it up for real now
 warning_ten_minutes_before=$((dead_zone_start_minutes - 10))
-if is_near $current_minutes $warning_ten_minutes_before 0; then
+if [[ $current_minutes == $warning_ten_minutes_before ]]; then
     echo "10 minutes before dead zone"
     say "Warning: The dead zone starts in 10 minutes. Wrap up your work now!"
 fi
